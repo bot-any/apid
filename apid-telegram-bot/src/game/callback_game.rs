23981cb6cd7b1324ebd3d9a8f0f24ca78b2c474a
RanolP <public.ranolp@gmail.com>
2022-09-09 22:04:36 +0900

    CallbackGame

---
+++ apid-telegram-bot/src/game/callback_game.rs
@@ -0,0 +1,5 @@
+use serde::{Deserialize, Serialize};
+
+/// A placeholder, currently holds no information. Use BotFather to set up your game.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CallbackGame;
--- apid-telegram-bot/src/game/mod.rs
+++ apid-telegram-bot/src/game/mod.rs
@@ -1,3 +1,5 @@
+pub use callback_game::*;
 pub use game::*;
 
+mod callback_game;
 mod game;
