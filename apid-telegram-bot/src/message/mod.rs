c131a422ab315048cab6b38d486aedb390e6854b
RanolP <public.ranolp@gmail.com>
2022-09-10 14:41:43 +0900

    Actually it is only message

--- apid-telegram-bot/src/message/mod.rs
+++ apid-telegram-bot/src/message/mod.rs
@@ -2,8 +2,10 @@ pub use dice::*;
 pub use message::*;
 pub use message_entity::*;
 pub use message_id::*;
+pub use video_chat::*;
 
 mod dice;
 mod message;
 mod message_entity;
 mod message_id;
+mod video_chat;
